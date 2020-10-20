564d688b407cf56749b04d983b0263fe051e0a4f
linyuwei <linyuwei.designer@gmail.com>
2020-10-20 21:19:44 +0800

    Updates

---
+++ precache-manifest.76d25f5c62461d5dc62f3865f8a5ab9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d657d6de2d2b1e1a35ae19dfcfe5b376",
+    "url": "/robotfriens-redux-pwa/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/robotfriens-redux-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b05ce538887e9361118",
+    "url": "/robotfriens-redux-pwa/static/css/main.4c37bd84.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/robotfriens-redux-pwa/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robotfriens-redux-pwa/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b05ce538887e9361118",
+    "url": "/robotfriens-redux-pwa/static/js/main.6181bcb8.chunk.js"
+  },
+  {
+    "revision": "674f7ee71c7393fe5692",
+    "url": "/robotfriens-redux-pwa/static/js/runtime-main.22d8fb19.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriens-redux-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
